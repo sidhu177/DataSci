cfab85515f5e1ac5f756e96c16c573053521cf46
Sidhu177 <sidharth177@gmail.com>
2018-06-08 23:39:02 -0400

    Text parsing program that still to be tested

---
+++ textParsing.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun  8 23:31:53 2018
+
+parsing Text File Taken from "Beginning Python Visualization by Shai Vaingast, Apress"
+
+@author: SIDHARTH
+"""
+
+def srchfile(filename,substr):
+    lines = open(filename).readlines()
+    fmt = r'%' + str(int((len(lines)))+1)+r'd:%s'
+    for index, line in enumerate(lines):
+        if line.find(substr)!=-1:
+            try:
+                print(fmt % (index,line.rstrip()))
+            except UnicodeEncodeError:
+                print("%d: ---- encoding error ---" %index)
+                
