6c85f3a242f73fa0c7f27a2d2b544254a8b43f26
Sidhu177 <sidharth177@gmail.com>
2018-06-12 22:18:13 -0400

    Automate the file and folder structure

--- FolderWalk.py
+++ FolderWalk.py
@@ -2,14 +2,14 @@
 """
 Created on Tue Jun 12 22:06:40 2018
 
-Taken From Automate the boring Stuff using Python
+Taken From Automate the boring Stuff using Python, By Al Sweigart, No Starch Press, 2014 
 
 @author: SIDHARTH
 """
 
 import os
 
-for folderName, subfolders, filenames in os.walk('E:\\Yamaha'):
+for folderName, subfolders, filenames in os.walk('E:\\temp'):
     print('The current folder is ' + folderName)
     for subfolder in subfolders:
         print('SUBFOLDER OF' + folderName + ':' + subfolder)
