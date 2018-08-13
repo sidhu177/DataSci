23b6eb000f8fc2dc52e189b75ec98bc8491ab599
Sidhu177 <sidharth177@gmail.com>
2018-08-12 21:58:15 -0400

    Added Recursive power method

--- recursiveSum.py
+++ recursiveSum.py
@@ -11,10 +11,20 @@ def recursiveSum(n):
     else:
         return recursiveSum(n-1) + n
         
+def power(x,n):
+    if n==0:
+        return 1
+    else:
+        return x*power(x,n-1)
+        
 def main():
     x = int(input("Please enter a non-negetive integer: "))
     s = recursiveSum(x)
+    n = int(input("Please enter a non-negetive power index: "))
     print("The sum of the first",x,"integers is ", str(s)+".")
+    q = power(x,n)
+    print("The power result",str(q)+".")
+    
     
 if __name__ == "__main__":
     main()
