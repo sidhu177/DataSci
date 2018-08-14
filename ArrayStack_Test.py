4cb31d5f44e03d98b3706c2563e81455d1a54509
Sidhu177 <sidharth177@gmail.com>
2018-08-13 23:39:53 -0400

    Stack datastructure example in python

---
+++ ArrayStack_Test.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 13 23:36:33 2018
+
+Array Stack Test
+
+@author: SIDHARTH
+"""
+
+S = ArrayStack()
+S.push(5)
+S.push(3)
+print(len(S))
+print(S.pop())
+print(S.is_empty())
